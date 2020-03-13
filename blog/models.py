95e564bff78cb6cfa1a474077cfadaf016e418cc
Adam John Benitez <adamjohndbenitez@outlook.com>
2020-03-12 17:02:06 -0800

    Setup Postgres settings.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Add the Blog app to the settings in INSTALLED_APPS
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
