c9cd52af4afef49db716e0f5af81dd2d7f6c1a44
Adam John Benitez <adamjohndbenitez@outlook.com>
2020-04-02 01:49:48 -0800

    first commit for blogs and jobs functionality

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,18 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
+    # this is to function gives 100 char only
+    def summary(self):
+        return self.body[:100]
+
+    # this function will show only date
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
 # Add the Blog app to the settings in INSTALLED_APPS
 
 # Create a migration
